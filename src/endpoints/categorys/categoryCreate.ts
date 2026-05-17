1fef25b0a6a297ee2d9d9bc9a00f1f73c6295621
sadisticus <and_johnston@hotmail.com>
2026-05-17 12:17:21 +1000

    Refactor CategoryCreate to use CategoryModel
    Removed the `Category` class dependency in `CategoryCreate` and updated `_meta` to use `CategoryModel` for `model` and `fields`. Simplified field selection logic by leveraging `CategoryModel.schema.pick`.
    
    Binary files `CodeChunks.db-wal` and `SemanticSymbols.db-wal` were modified, but specific changes are not detailed as they are binary files.

--- src/endpoints/categorys/categoryCreate.ts
+++ src/endpoints/categorys/categoryCreate.ts
@@ -1,11 +1,11 @@
 ﻿import { D1CreateEndpoint } from "chanfana";
 import { HandleArgs } from "../../types";
-import { Category, CategoryModel } from "./base";
+import { CategoryModel } from "./base";
 
 export class CategoryCreate extends D1CreateEndpoint<HandleArgs> {
 	_meta = {
-		model: Category,
-		fields: Category.pick({
+		model: CategoryModel,
+		fields: CategoryModel.schema.pick({
 			// this is purposely missing the id, because users shouldn't be able to define it
             material_type_id: true,
 			name: true,
